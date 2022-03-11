f2a208422dc9aa43933d6077abf2331da8a62f7d
Vitoria Borba <vitoriaborba@icloud.com>
2022-03-11 15:04:12 +0000

    Navbar page setup

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {NavLink} from 'react-router-dom'
+import { AuthContext } from '../../context/auth.context'
 
 function NavBar() {
+  const {loggedIn, user, logoutUser} = useContext(AuthContext);
   return (
-    <nav>
-      <NavLink to='/user/:id'>Profile</NavLink>
-      <NavLink to='/users'>Community</NavLink>
+    <div>
+      {loggedIn && (
+       <nav>
       <NavLink to='/feed'>Want to Help?</NavLink>
+      <NavLink to='/users'>Community</NavLink>
+      <NavLink to='/user/:id'>Profile</NavLink>
     </nav>
+    )}
+    </div>
   )
 }
 
