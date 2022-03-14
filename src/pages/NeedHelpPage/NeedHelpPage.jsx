b8f60f3eb6d241262024e56cfc68ca06b37699ca
filipanatalio <87820213+filipanatalio@users.noreply.github.com>
2022-03-14 13:20:47 +0000

    SearchBar locations created

--- src/pages/NeedHelpPage/NeedHelpPage.jsx
+++ src/pages/NeedHelpPage/NeedHelpPage.jsx
@@ -1,13 +1,66 @@
-import React from 'react'
-
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Select from 'react-select';
 
 function NeedHelpPage() {
+  const [locationInput, setLocationInput] = useState('');
 
+  const [getLocation, setGetLocation] = useState('');
+
+  const locationOptions = []
+
+  useEffect(() => {
+    axios
+      .get(`https://geoptapi.org/municipio?`)
+      .then((response) => {
+        console.log(response.data);
+        setLocationInput(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [getLocation]);
+
+  const handleLocationInput = (e) => setLocationInput(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setGetLocation(locationInput);
+    // console.log(getLocation);
+  };
+
+  if(locationInput) {
+      locationInput.forEach((location) => {
+        const option = { value:`${location.toLowerCase()}`, label:`${location}`}
+        locationOptions.push(option)
+      })
+  }
+
+  console.log(locationOptions)
+
+  //UseState
+  //Render
+  //Useffect (fetching)
+  //Changing state
+  //Component re-renders
 
   return (
-    <div>NeedHelpPage</div>
-  )
+    <div>
+      
+      {/* <Select onSubmit={handleSubmit}>
+        <input type="text" name="locations" onChange={handleLocationInput} />
+        <button>Search</button>
+      </Select>
+      {locations ? (
+        <>
+          <h1>{locations}</h1>
+      
+        </>
+      ) : (
+        <h1>No locations found</h1>
+      )} */}
+      <Select options={locationOptions} />
+    </div>
+  );
 }
 
 export default NeedHelpPage
